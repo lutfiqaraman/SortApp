d5ed932cb2754c875bb9776f78d02dd1334ac29b
lutfiqaraman <lutfiqaraman@gmail.com>
2021-06-20 21:04:21 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SortApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[4] { 9, 0, 3, 1 };
+            int temp = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (numbers[j] < numbers[i])
+                    {
+                        temp = numbers[i];
+                        numbers[i] = numbers[j];
+                        numbers[j] = temp;
+                    }
+
+                }
+
+                Console.Write(numbers[i] + "\t");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
